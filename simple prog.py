a20bdbffd80bc18a142521884a3d39c618ca8d37
saikatbala21 <saikatbala21.sm@gmail.com>
2025-07-08 19:04:52 +0530

    Add files via upload

---
+++ simple prog.py
@@ -0,0 +1,12 @@
+a=input("enter the first no")
+a=int(a)
+b=input("enter the second no")
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addition:",c)
+print("substraction:",d)
+print("Multiplication:",e)
+print("division:",f)
---
+++ simple project name add.py
@@ -0,0 +1,5 @@
+a=input("enter your first name:")
+b=input("enter your last name:")
+c=a+b
+print("Hellow")
+print(c,"welcome to the python program")
